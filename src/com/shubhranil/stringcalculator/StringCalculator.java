5cbb36a3737672caad49904303fcdb4a197d4326
alpha037 <subhronil.dutta@gmail.com>
2021-09-13 23:22:59 +0530

    UPDATE: Test calculator with multiple numbers and multiple delimeters

--- src/com/shubhranil/stringcalculator/StringCalculator.java
+++ src/com/shubhranil/stringcalculator/StringCalculator.java
@@ -1,12 +1,32 @@
 package com.shubhranil.stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+	private final String DEFAULT_DELIMITER_REGEX = "[,\n]";
+	private final String MULTIPLE_DELIMITER_REGEX = "//(.*)\n(.*)";
 	
 	public int add(String numbers) {
 		if (numbers.equals("")) return 0;
 		
-		String[] numbersList = numbers.split("[,\n]");
+		String[] numbersList = split(numbers);
 		return sum(numbersList);
+	}
+	
+	private String[] split(String numbers) {
+		String delimiter = DEFAULT_DELIMITER_REGEX;
+		
+		if (numbers.startsWith("//")) {
+			Pattern pattern = Pattern.compile(MULTIPLE_DELIMITER_REGEX);
+			Matcher matcher = pattern.matcher(numbers);
+			if (matcher.find()) {
+				delimiter = matcher.group(1);
+				numbers = matcher.group(2);
+			}
+		}
+		
+		return numbers.split(delimiter);
 	}
 	
 	private int sum(String[] numbersList) {
--- src/com/shubhranil/stringcalculator/StringCalculatorTest.java
+++ src/com/shubhranil/stringcalculator/StringCalculatorTest.java
@@ -29,4 +29,9 @@ public class StringCalculatorTest {
 	public void testWithNewLineDelimeter() {
 		assertEquals(6, new StringCalculator().add("1\n2,3"));
 	}
+	
+	@Test
+	public void testWithMultipleDelimiters() {
+		assertEquals(3, new StringCalculator().add("//;\n1;2"));
+	}
 }
